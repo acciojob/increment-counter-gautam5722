314c05baf25bdfac50b410c1e45a6f1c194a30b5
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2024-12-16 19:53:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let counter = 0;
+const counterValue = document.getElementById("counter");
+const increment = document.getElementById("incrementBtn");
+
+increment.addEventListener('click', () => {
+	counter++;
+	alert(counter);
+	counterValue.innerHTML = counter;
+})
